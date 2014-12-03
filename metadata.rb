596f86c24d89ae819d94dfc9d35db16a72b02e7a
Igor Kurochkin <ikurochkin@express42.com>
2014-12-03 19:05:14 +0300

    Version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
-name              "redis"
-maintainer        "LLC Express 42"
-maintainer_email  "info@express42.com"
-license           "MIT"
-description       "Installs and configures redis instanses"
-version           "0.1.1"
+name              'redis'
+maintainer        'LLC Express 42'
+maintainer_email  'info@express42.com'
+license           'MIT'
+description       'Provides LWRP for managing multiple redis instances.'
+version           '0.1.2'
 
-depends           "runit"
-supports          "debian"
+depends           'runit'
+
+supports          'debian'
+supports          'ubuntu'
