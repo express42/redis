440f5b06705af7483f209d87af5fd17be6310f57
Igor Kurochkin <ikurochkin@express42.com>
2014-12-11 14:31:48 +0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'redis'
 maintainer        'LLC Express 42'
-maintainer_email  'info@express42.com'
+maintainer_email  'cookbooks@express42.com'
 license           'MIT'
 description       'Provides LWRP for managing multiple redis instances.'
 version           '0.1.2'
@@ -10,4 +10,4 @@ depends           'runit'
 supports          'debian'
 supports          'ubuntu'
 
-recipe            'postfix::default', 'Do nothing'
+recipe            'redis::default', 'Do nothing.'
