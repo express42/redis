fae91e712bfb58d87bb53638a6a544f8a1a2135e
Nklya <nklya@hotmail.com>
2017-04-03 15:52:11 +0300

    Switch to cookstyle+set chef version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,10 @@ maintainer        'LLC Express 42'
 maintainer_email  'cookbooks@express42.com'
 license           'MIT'
 description       'Provides LWRP for managing multiple redis instances.'
+chef_version      '>= 12'
 version           '0.1.2'
+source_url 'https://github.com/express42-cookbooks/redis' if respond_to?(:source_url)
+issues_url 'https://github.com/express42-cookbooks/redis/issues' if respond_to?(:issues_url)
 
 depends           'runit'
 
